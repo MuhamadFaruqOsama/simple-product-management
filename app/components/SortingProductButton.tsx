08cef841f2fce0758e9899ec8bf486abf336f11d
muhamadfaruqosama <abdolslamet@gmail.com>
2026-07-28 14:30:19 +0700

    change soring button color

--- app/components/SortingProductButton.tsx
+++ app/components/SortingProductButton.tsx
@@ -8,7 +8,7 @@ export function SortingProductButton() {
     const [isDescOrder, setIsDescOrder] = useState(false)
 
     return (
-        <button onClick={() => setIsDescOrder(!isDescOrder)} className="px-2 bg-orange-500 text-white rounded-md cursor-pointer">
+        <button onClick={() => setIsDescOrder(!isDescOrder)} className="px-2 bg-gray-200 text-gray-800 rounded-md cursor-pointer">
             {
                 isDescOrder ? 
                 (<HugeiconsIcon icon={Sorting01Icon} />) :
